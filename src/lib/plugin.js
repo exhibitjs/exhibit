a0cd752301c368e50e7a847b9dd369acb94db836
Callum Locke <callum.locke@gmail.com>
2016-04-03 16:15:32 +0100

    make plugin function work

--- src/lib/plugin.js
+++ src/lib/plugin.js
@@ -6,16 +6,26 @@
  * - `require('exhibit-plugin-foo')(1, 2)`
  */
 
+import stackTrace from 'stack-trace';
+import resolveFrom from 'resolve-from';
+import path from 'path';
+import { cyan, red } from 'chalk';
+
 export default function plugin(name, ...args) {
-  let loadedPlugin;
-  try {
-    loadedPlugin = require(`exhibit-plugin-${name}`);
-  }
-  catch (error) {
-    // TODO format 'can't find module' errors nicely, with suggested solution
+  const callingFile = stackTrace.get()[1].getFileName();
+  const pluginPath = resolveFrom(path.dirname(callingFile), `exhibit-plugin-${name}`);
 
-    throw error;
+  if (!pluginPath) {
+    console.error(
+      red(`\n\nCould not find plugin: ${name}`) +
+      '\n\nIf this plugin is published on npm, you can install it like this:\n\n' +
+      cyan(`  npm install -D exhibit-plugin-${name}\n\n`)
+    );
+
+    throw new Error(`Exhibit plugin not found: exhibit-plugin-${name}`);
   }
 
-  return loadedPlugin(...args);
+  const loadedPlugin = require(pluginPath);
+
+  return (loadedPlugin.default || loadedPlugin)(...args);
 }
